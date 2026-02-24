2d7ed3c6b11da41b937cba02a72c00241f3d7973
Thridsanu <6810110147@psu.ac.th>
2026-02-24 15:31:36 +0700

    homepage and player-screen

---
+++ screens/home_screen.py
@@ -0,0 +1,3 @@
+def open_player(self, song_id):
+    print(f"Opening song: {song_id}")
+    self.manager.current = 'player' 
---
+++ screens/player_screen.py
@@ -0,0 +1,5 @@
+def back_home(self):
+    self.manager.current = 'home'
+
+def on_play(self):
+    print("Music Playing...") # ยังไม่ต้องใส่เสียงจริงตาม Step 0
